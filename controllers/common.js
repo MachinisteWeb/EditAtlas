be10164ad8238284d88f06358c5a48a917f10652
Bruno Lesieur <bruno.lesieur@gmail.com>
2016-12-23 13:33:02 +0100

    Full compatibility with NodeAtlas v2.0
    Signed-off-by: Bruno Lesieur <bruno.lesieur@gmail.com>

--- controllers/common.js
+++ controllers/common.js
@@ -17,45 +17,44 @@ website.components = {};
         });
     };
 
-    publics.changeVariations = function (params, next) {
+    publics.changeVariations = function (next, locals) {
         var NA = this,
-            variations = params.variations,
-            template = variations.common.demo.template[0],
+            template = locals.common.demo.template[0],
             object;
 
-        variations.file = variations.routeParameters.variation;
-        variations.fs = ((variations.languageCode) ? variations.languageCode + "/": "") + variations.routeParameters.variation;
-        variations.fc = ((variations.languageCode) ? variations.languageCode + "/": "") + variations.webconfig.commonVariation;
+        locals.file = locals.routeParameters.variation;
+        locals.fs = ((locals.languageCode) ? locals.languageCode + "/": "") + locals.routeParameters.variation;
+        locals.fc = ((locals.languageCode) ? locals.languageCode + "/": "") + locals.webconfig.commonVariation;
 
-        variations.common.demo.template = [];
+        locals.common.demo.template = [];
 
         function replaceNames(str, i) {
             return str
-                .replace(/%firstname%/g, variations.common.demo.list[i].firstname)
-                .replace(/%lastname%/g, variations.common.demo.list[i].lastname);
+                .replace(/%firstname%/g, locals.common.demo.list[i].firstname)
+                .replace(/%lastname%/g, locals.common.demo.list[i].lastname);
         }
         function replaceCompany(str, i) {
             return str
-                .replace(/%company%/g, variations.common.demo.list[i].company);
+                .replace(/%company%/g, locals.common.demo.list[i].company);
         }
         function replaceImage(str, i) {
             return replaceCompany(replaceNames(str, i), i);
         }
 
-        for (var i = 0; i < variations.common.demo.list.length; i++) {
+        for (var i = 0; i < locals.common.demo.list.length; i++) {
             object = {};
             object.name = replaceNames(template.name, i);
             object.company = replaceCompany(template.company, i);
             object.image = {};
-            object.image.src = variations.common.demo.list[i].image;
+            object.image.src = locals.common.demo.list[i].image;
             object.image.alt = replaceImage(template.image.alt, i);
-            variations.common.demo.template.push(object);
+            locals.common.demo.template.push(object);
         }
 
-        variations = website.components.editAtlas.setFilters.call(NA, variations);
-        variations = website.components.componentAtlas.includeComponents.call(NA, variations, "components", "mainTag");
+        locals = website.components.editAtlas.setFilters.call(NA, locals);
+        locals = website.components.componentAtlas.includeComponents.call(NA, locals, "components", "mainTag");
 
-        next(variations);
+        next();
     };
 
 }(website));
